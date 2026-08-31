031a4a84e5bf273924a7757b8ec6bbe14889c7ab
razannabilWebDev <razannabilannadif@gmail.com>
2026-08-31 15:32:40 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])
+  ->name('dashboard');
